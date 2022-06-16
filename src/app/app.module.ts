000b67367dd58db767aa5796b099e415288f8ca7
Olga Shvets <olgashvez84@gmail.com>
2022-06-16 18:17:26 +0300

    init project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,15 @@
+import { StopwatchComponent } from './stopwatch/stopwatch.component';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StopwatchComponent
   ],
   imports: [
     BrowserModule,
